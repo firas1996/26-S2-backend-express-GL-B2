3ce4153038f2bd82095d6a8dc8cc45005271bacc
firas1996 <firas.hm.1996@gmail.com>
2026-02-20 11:25:50 +0100

    aa

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-
+const userRoutes = require("./routes/userRoutes");
 dotenv.config({ path: "./.env" });
 
 mongoose
@@ -14,6 +14,8 @@ mongoose
   });
 
 const app = express();
+
+app.use(userRoutes);
 
 const port = 1122;
 
