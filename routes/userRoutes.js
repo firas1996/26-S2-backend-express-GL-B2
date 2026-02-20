42eeaadddfc57c285ea292f266e6cfb774f7e070
firas1996 <firas.hm.1996@gmail.com>
2026-02-20 12:06:39 +0100

    aa

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,8 @@
-const { createUser } = require("../controllers/userController");
+const { createUser, getAllUsers } = require("../controllers/userController");
 
 const router = require("express").Router();
 
 router.post("/createuser", createUser);
+router.get("/getUsers", getAllUsers);
 
 module.exports = router;
